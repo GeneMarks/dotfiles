dfd18b8c484a0de8956185c8834dab2390bea24a
Gene Marks <genemarks74@gmail.com>
2024-03-26 09:29:38 -0400

    Add clangd to lsp install list

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -23,6 +23,7 @@ return {
         require("mason").setup({})
         require("mason-lspconfig").setup({
             ensure_installed = {
+                "clangd",
                 "tsserver",
                 "eslint",
                 "lua_ls"
