67e9dd6ca875bab7b09474e5b9304dc48e0f3496
Gene Marks <genemarks74@gmail.com>
2024-03-30 09:56:47 -0400

    Fix formatting

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -3,12 +3,12 @@ return {
     build = ":TSUpdate",
     config = function()
         require("nvim-treesitter.install").compilers = { "clang" }
-        require("nvim-treesitter").setup {
+        require("nvim-treesitter").setup({
             ensure_installed = { "lua", "vim", "vimdoc", "query", "cpp", "java", "javascript", "typescript", "html", "css" },
             sync_install = false,
             auto_install = true,
             highlight = { enable = true },
-            -- indent = { enable = true },  
-        }
+            -- indent = { enable = true },
+        })
     end
 }
