200c2385fd7e1c61c2b7baca72495f7f245ce8d3
Gene Marks <genemarks74@gmail.com>
2024-03-30 09:57:16 -0400

    Add telescope-undo extension plugin

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,8 +1,18 @@
 return {
     "nvim-telescope/telescope.nvim", tag = "0.1.4",
-    dependencies = { "nvim-lua/plenary.nvim" },
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "debugloop/telescope-undo.nvim"
+    },
+    config = function ()
+        local telescope = require("telescope")
+        telescope.setup({
+            extensions = {
+                undo = {},
+            },
+        })
+        telescope.load_extension("undo")
 
-    config = function () 
         local wk = require("which-key")
         local builtin = require("telescope.builtin")
 
@@ -10,6 +20,7 @@ return {
         vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
         vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
         vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+        vim.keymap.set("n", "<leader>fu", "<CMD>Telescope undo<CR>")
 
         wk.register({
             f = {
@@ -17,7 +28,8 @@ return {
                 f = "Find files",
                 g = "Live grep",
                 b = "Search buffers",
-                h = "Search help tags"
+                h = "Search help tags",
+                u = "Undo history",
             }
         }, { prefix = "<leader>" })
     end
