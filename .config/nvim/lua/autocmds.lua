6f595d66341c2da3201fdfdd2d8c1ac79d8d3840
Gene Marks <genemarks74@gmail.com>
2024-03-31 00:01:46 -0400

    Add autocmd to enter insert mode on terminal buffers

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -1,11 +1,15 @@
+-- Enter insert mode on terminal buffers
+vim.api.nvim_create_autocmd("BufEnter", {
+    pattern = "term://*",
+    command = "startinsert",
+})
+
 -- Highlight on yank
 local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
 vim.api.nvim_create_autocmd("TextYankPost", {
+    pattern = "*",
     callback = function()
         vim.highlight.on_yank()
     end,
     group = highlight_group,
-    pattern = "*",
 })
-
-
