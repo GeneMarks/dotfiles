c90f943fd0f90380311ab9e6a8d841c2f48ca550
Gene Marks <genemarks74@gmail.com>
2024-03-31 09:51:58 -0400

    Add keymap for no-neck-pain toggle

--- .config/nvim/lua/plugins/no-neck-pain.lua
+++ .config/nvim/lua/plugins/no-neck-pain.lua
@@ -1,19 +1,24 @@
 return {
     "shortcuts/no-neck-pain.nvim", version = "*",
-    opts = {
-        width = 120,
-        autocmds = {
-            enableOnVimEnter = true,
-            enableOnTabEnter = true,
-            skipEnteringNoNeckPainBuffer = true,
-        },
-        buffers = {
-            scratchPad = {
-                enabled= false
+    config = function()
+        require("no-neck-pain").setup({
+            width = 120,
+            autocmds = {
+                enableOnVimEnter = true,
+                enableOnTabEnter = true,
+                skipEnteringNoNeckPainBuffer = true,
             },
-            wo = {
-                fillchars = "eob: "
+            buffers = {
+                scratchPad = {
+                    enabled= false
+                },
+                wo = {
+                    fillchars = "eob: "
+                }
             }
-        }
-    }
+        })
+        local wk = require("which-key")
+        vim.keymap.set("n", "<leader>n", "<CMD>NoNeckPain<CR>")
+        wk.register({ ["<leader>n"] = "No Neck Pain" })
+    end
 }
