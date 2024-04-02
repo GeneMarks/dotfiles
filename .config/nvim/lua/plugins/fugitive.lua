1b72f99c2f2843d0afee70d52fe69d3b5b46b8eb
Gene Marks <genemarks74@gmail.com>
2024-04-02 14:11:17 -0400

    Add keymap for Git fugitive command

--- .config/nvim/lua/plugins/fugitive.lua
+++ .config/nvim/lua/plugins/fugitive.lua
@@ -1,3 +1,8 @@
 return {
     "tpope/vim-fugitive",
+    config = function()
+        local wk = require("which-key")
+        vim.keymap.set("n", "<leader>g", "<CMD>Git<CR>")
+        wk.register({ ["<leader>g"] = "Fugitive" })
+    end
 }
