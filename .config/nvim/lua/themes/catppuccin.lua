7795feb519cbe0b8bb7d9be533cdb42eff74f47e
Gene Marks <genemarks74@gmail.com>
2024-04-02 10:03:41 -0400

    Override MatchParen bg color for catppuccin-latte

--- .config/nvim/lua/themes/catppuccin.lua
+++ .config/nvim/lua/themes/catppuccin.lua
@@ -3,4 +3,15 @@ return {
     name = "theme-catppuccin",
     lazy = false,
     priority = 1000,
+    config = function()
+        require("catppuccin").setup({
+            highlight_overrides = {
+                latte = function(latte)
+                    return {
+                        MatchParen = { bg = "#e2e4e9" }
+                    }
+                end
+            }
+        })
+    end
 }
