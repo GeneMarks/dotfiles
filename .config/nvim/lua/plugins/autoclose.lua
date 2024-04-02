f1fa1f87715bff96da611c0949f895466f0b4dbb
Gene Marks <genemarks74@gmail.com>
2024-04-02 15:12:45 -0400

    Enable disable_command_mode

--- .config/nvim/lua/plugins/autoclose.lua
+++ .config/nvim/lua/plugins/autoclose.lua
@@ -2,7 +2,8 @@ return {
     "m4xshen/autoclose.nvim",
     opts = {
         options = {
-            pair_spaces = true
+            pair_spaces = true,
+            disable_command_mode = true,
         }
     }
 }
