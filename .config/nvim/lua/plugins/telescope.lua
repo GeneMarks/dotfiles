99a9fa1f5a66b5b6b8bfe1d9e964ad6f7a8e2c74
Gene Marks <genemarks74@gmail.com>
2024-04-02 14:55:24 -0400

    Add auto-session plugin and telescope extension

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -2,17 +2,24 @@ return {
     "nvim-telescope/telescope.nvim", tag = "0.1.6",
     dependencies = {
         "nvim-lua/plenary.nvim",
-        "debugloop/telescope-undo.nvim"
+        "debugloop/telescope-undo.nvim",
+        "rmagatti/session-lens",
+        "rmagatti/auto-session",
     },
     config = function ()
+        -- Configs
         local telescope = require("telescope")
         telescope.setup({
             extensions = {
                 undo = {},
             },
         })
-        telescope.load_extension("undo")
 
+        -- Load extensions
+        telescope.load_extension("undo")
+        telescope.load_extension("session-lens")
+
+        -- Keymaps
         local wk = require("which-key")
         local builtin = require("telescope.builtin")
 
@@ -21,6 +28,7 @@ return {
         vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
         vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
         vim.keymap.set("n", "<leader>fu", "<CMD>Telescope undo<CR>")
+        vim.keymap.set("n", "<leader>fs", "<CMD>SearchSession<CR>")
 
         wk.register({
             f = {
@@ -29,7 +37,8 @@ return {
                 g = "Live grep",
                 b = "Search buffers",
                 h = "Search help tags",
-                u = "Undo history",
+                u = "Search undo history",
+                s = "Search sessions",
             }
         }, { prefix = "<leader>" })
     end
