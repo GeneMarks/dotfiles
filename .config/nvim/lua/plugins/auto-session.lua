83725deeb1c76524fac99d62fc3971ec72180e31
Gene Marks <genemarks74@gmail.com>
2024-04-03 22:32:12 -0400

    Add fugitive to bypass_session_save_file_types

--- .config/nvim/lua/plugins/auto-session.lua
+++ .config/nvim/lua/plugins/auto-session.lua
@@ -1,9 +1,10 @@
 return {
     "rmagatti/auto-session",
-    config = function()
-        require("auto-session").setup({
-            log_level = "error",
-            auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/"},
-        })
-    end
+    opts = {
+        log_level = "error",
+        auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/"},
+        bypass_session_save_file_types = {
+            "fugitive",
+        },
+    },
 }
