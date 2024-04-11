ad1b8672a3f39c0a4b65a0981a63b49e188457d6
Gene Marks <genemarks74@gmail.com>
2024-04-11 17:06:24 -0400

    Add languages to ensure_installed list

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,26 @@ return {
     config = function()
         require("nvim-treesitter.install").compilers = { "clang" }
         require("nvim-treesitter").setup({
-            ensure_installed = { "lua", "vim", "vimdoc", "query", "cpp", "java", "javascript", "typescript", "html", "css" },
+            ensure_installed = {
+                "c",
+                "cpp",
+                "css",
+                "cmake",
+                "dockerfile",
+                "gitignore",
+                "html",
+                "java",
+                "javascript",
+                "json",
+                "lua",
+                "markdown",
+                "markdown_inline",
+                "query",
+                "tsx",
+                "typescript",
+                "vim",
+                "vimdoc",
+            },
             sync_install = false,
             auto_install = true,
             highlight = { enable = true },
