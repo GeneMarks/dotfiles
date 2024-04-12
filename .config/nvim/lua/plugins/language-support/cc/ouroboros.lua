593691f89c018d6bd54a695f34a577e8b2cf3313
Gene Marks <genemarks74@gmail.com>
2024-04-12 11:00:47 -0400

    Add ouroboros plugin

---
+++ .config/nvim/lua/plugins/language-support/cc/ouroboros.lua
@@ -0,0 +1,18 @@
+return {
+    "jakemason/ouroboros.nvim",
+    dependencies = "nvim-lua/plenary.nvim",
+    config = function()
+        require("ouroboros").setup({})
+
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = {"c", "cpp"},
+            callback = function()
+                vim.api.nvim_buf_set_keymap(0, 'n', '<C-e>', '<CMD>Ouroboros<CR>', {
+                    desc = "Switch h/cpp files",
+                    noremap = true,
+                    silent = true
+                })
+            end,
+        })
+    end
+}
