6bc30e4b25c61d281ace206e6c03a099a7e14193
Gene Marks <genemarks74@gmail.com>
2024-04-14 00:22:11 -0400

    Refactor lazy install & setup

--- .config/nvim/lua/lazy-setup.lua
+++
@@ -1,8 +0,0 @@
-require("lazy").setup({
-    spec = {
-        { import = "themes" },
-        { import = "plugins" },
-        { import = "plugins.lsp" },
-        { import = "plugins.language-support.cc" },
-    }
-})
