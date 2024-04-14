b4e6a6b708f0c9f7a06a2b8acf54749f4648f5ea
Gene Marks <genemarks74@gmail.com>
2024-04-13 23:58:59 -0400

    Move cmp config to cmp.lua

--- .config/nvim/lua/plugins/lsp/lsp.lua
+++ .config/nvim/lua/plugins/lsp/lsp.lua
@@ -3,11 +3,6 @@ return {
     dependencies = {
         "williamboman/mason.nvim",
         "williamboman/mason-lspconfig.nvim",
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-cmdline",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/nvim-cmp",
-        "L3MON4D3/LuaSnip",
     },
     config = function ()
         local servers = {
@@ -18,7 +13,7 @@ return {
             "lua_ls"
         }
 
-        -- Keymaps
+        -- Lsp keymaps
         vim.api.nvim_create_autocmd("LspAttach", {
             desc = "LSP actions",
             callback = function(event)
@@ -69,88 +64,6 @@ return {
         require('mason-lspconfig').setup({
             ensure_installed = servers,
             handlers = handlers
-        })
-
-        -- Cmp config
-        local cmp = require("cmp")
-        local luasnip = require("luasnip")
-
-        local lsjump = function(direction)
-            return cmp.mapping(function(fallback)
-                if luasnip.jumpable(direction) then
-                    luasnip.jump(direction)
-                else
-                    fallback()
-                end
-            end, {'i', 's'})
-        end
-
-        cmp.setup({
-            mapping = cmp.mapping.preset.insert({
-                ["<CR>"] = cmp.mapping.confirm({ select = true }),
-                ["<C-u>"] = cmp.mapping.scroll_docs(-4),
-                ["<C-d>"] = cmp.mapping.scroll_docs(4),
-                ["<C-SPACE>"] = cmp.mapping.complete(),
-                ["<C-l>"] = lsjump(1),
-                ["<C-h>"] = lsjump(-1),
-            }),
-            -- preselect = "item",
-            -- completion = {
-            --     completeopt = "menu, menuone, noinsert"
-            -- },
-            -- window = {
-            --     completion = cmp.config.window.bordered(),
-            --     documentation = cmp.config.window.bordered(),
-            -- },
-            sources = {
-                { name = "nvim_lsp" },
-                { name = "buffer" },
-            },
-            formatting = {
-                fields = { "abbr", "kind", "menu" },
-                format = function(entry, item)
-                    local source = entry.source.name
-                    local formatted = {
-                        nvim_lsp = "LSP",
-                        buffer = "Buffer",
-                        path = "Path",
-                        cmdline = "CMD",
-                    }
-                    item.menu = " [" .. formatted[source] .. "]"
-
-                    return item
-                end,
-            },
-            snippet = {
-                expand = function(args)
-                    luasnip.lsp_expand(args.body)
-                end,
-            },
-            experimental = {
-                ghost_text = true,
-            },
-        })
-
-        cmp.setup.cmdline("/", {
-            mapping = cmp.mapping.preset.cmdline(),
-            sources = {
-                { name = "buffer" },
-            },
-        })
-
-        cmp.setup.cmdline(":", {
-            mapping = cmp.mapping.preset.cmdline(),
-            sources = cmp.config.sources({
-                { name = "path" }
-            },
-            {
-                {
-                    name = "cmdline",
-                    option = {
-                        ignore_cmds = { "Man", "!" },
-                    },
-                }
-            })
         })
     end
 }
