5b029a55c5f740464e6982ac38c4189d12411a4a
Gene Marks <genemarks74@gmail.com>
2024-04-15 12:51:38 -0400

    Update clangd config

--- .config/nvim/lua/plugins/lsp/language-configs/clangd.lua
+++ .config/nvim/lua/plugins/lsp/language-configs/clangd.lua
@@ -4,10 +4,8 @@ return {
         "--all-scopes-completion",
         "--background-index",
         "--clang-tidy",
-        "--compile_args_from=filesystem",
         "--completion-style=detailed",
         "--fallback-style=llvm",
-        "--function-arg-placeholders",
         "--header-insertion=iwyu",
     }
 }
