0f82c21a85b84566a7f7a54fcefdfddc07b1c4a8
Gene Marks <genemarks74@gmail.com>
2024-04-17 11:20:03 -0400

    Explicitly use full bin paths

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,4 @@
-alias clang=$CC
-alias clang++="$CXX -std=c++20"
-alias g++="g++-13"
+alias clang="/opt/homebrew/opt/llvm/bin/clang"
+alias clang++="/opt/homebrew/opt/llvm/bin/clang++ -std=c++20"
+alias gcc="/opt/homebrew/opt/gcc/bin/gcc-13"
+alias g++="/opt/homebrew/opt/gcc/bin/g++-13"
