fd7a9a90db30f6a55fe9878c9df3619c6dcf06cc
Gene Marks <genemarks74@gmail.com>
2024-05-15 17:18:35 -0400

    Add ~/Desktop to auto_session_suppress_dirs

--- .config/nvim/lua/plugins/auto-session.lua
+++ .config/nvim/lua/plugins/auto-session.lua
@@ -2,7 +2,13 @@ return {
     "rmagatti/auto-session",
     opts = {
         log_level = "error",
-        auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/"},
+        auto_session_suppress_dirs = {
+            "~/",
+            "~/Projects",
+            "~/Desktop",
+            "~/Downloads",
+            "/",
+        },
         bypass_session_save_file_types = {
             "fugitive",
         },
