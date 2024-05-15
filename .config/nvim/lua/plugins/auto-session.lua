fe482b83e9f1577de51befa0c2de36b3cd56b978
Gene Marks <genemarks74@gmail.com>
2024-05-15 17:28:58 -0400

    use auto_session_allowed_dirs instead of suppressed

--- .config/nvim/lua/plugins/auto-session.lua
+++ .config/nvim/lua/plugins/auto-session.lua
@@ -2,12 +2,10 @@ return {
     "rmagatti/auto-session",
     opts = {
         log_level = "error",
-        auto_session_suppress_dirs = {
-            "~/",
-            "~/Projects",
-            "~/Desktop",
-            "~/Downloads",
-            "/",
+        auto_session_allowed_dirs = {
+            "/Volumes/T7/Development/projects/*",
+            "~/.dotfiles/*",
+            "~/.local/share/nvim/sessions"
         },
         bypass_session_save_file_types = {
             "fugitive",
