200f099524b9a075ce0a18898d7ca1704987edf1
Gene Marks <genemarks74@gmail.com>
2024-06-18 21:40:22 -0400

    update environment variables

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,3 +2,5 @@ alias clang="/opt/homebrew/opt/llvm/bin/clang"
 alias clang++="/opt/homebrew/opt/llvm/bin/clang++ -std=c++20"
 alias gcc="/opt/homebrew/opt/gcc/bin/gcc-13"
 alias g++="/opt/homebrew/opt/gcc/bin/g++-13"
+
+export PATH="$PATH:$HOME/.local/bin"
